b7eb1120cf8f1a4c0b522e2f8ddeed58a036e055
Amanda Reyes <amanda_reyesb@hotmail.com>
2023-05-08 12:06:42 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+REACT REDUX CRUD 
+
+1. "Rome" como formatter en lugar de ESLint 
+2. Tremor como coleccion de componentes pensados para formularios y dashboards. Tremor necesita tailwind, mas no vamos a usar directamente Tailwind.
+
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import ListOfUsers from "./components/ListOfUsers";
+
+function App() {
+	return (
+		<main>
+			<h1>Nuestro primer proyecto con Redux</h1>
+			<ListOfUsers />
+		</main>
+	);
+}
+
+export default App;
